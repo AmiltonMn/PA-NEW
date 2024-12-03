d79014d617e2e1696c31496735e22c7c51fb890e
AmiltonMn <amilton.moreiran@hotmail.com>
2024-12-03 16:57:19 -0300

    Agora temos o get da querida da API da Disney!

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from 'react';
 import axios from "axios"
 
 interface Character {
-  id: number,
+  _id: number,
   name: string,
-  image: string
+  imageUrl: string
 }
 
 export default function TabTwoScreen() {
@@ -15,8 +15,8 @@ export default function TabTwoScreen() {
   const [page, setPage] = useState<string>("1")
 
   const fetchCharacters = async (pageNumber: string) => {
+
     try {
-      
       const response = await axios.get(
         `https://api.disneyapi.dev/character?page=${pageNumber}`
       )
@@ -34,21 +34,104 @@ export default function TabTwoScreen() {
   }, [])
 
   const renderCharacter = ({item} : {item: Character}) => (
-    <View>
-      <Image source={{uri: item.image}}/>
-      <View style>
-        <Text>{item.name}</Text>
+    <View style={styles.card}>
+      <Image source={{uri: item.imageUrl}} style={styles.image}/>
+      <View style={styles.info}>
+        <Text style={styles.name}>{item.name}</Text>
       </View>
     </View>
   )
+
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color="#20F6CFFF"/>
+      </View>
+    )
+  }
   
   return (
-    <View>
-
+    <View style={{ flex: 1 }}>
+      <View style={styles.inputContainer}>
+        <Text>1/149 - </Text>
+        <TextInput
+          style={styles.input}
+          value={page}
+          onChangeText={(text) => setPage(text)}
+          keyboardType='numeric'
+          placeholder='Digite o número da página'
+        />
+        <Button title='Buscar' onPress={() => fetchCharacters(page)} />
+      </View>
+      <FlatList 
+        data={characters}
+        keyExtractor={(item) => item._id.toString()}
+        renderItem={renderCharacter}
+        contentContainerStyle={styles.list}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  card: {
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#F9F9F9FF",
+    marginBottom: 12,
+    borderRadius: 8,
+    width: 'auto',
+    overflow: "hidden",
+    elevation: 2, // * Sombra pra android
 
+    // * Abaixo sombra para IOS
+    // * link para criar a sombra https://ethercreative.github.io/react-native-shadow-generator/
+    shadowColor: "#000000",
+    shadowOpacity: 0.1,
+    shadowOffset: {
+      width: 0, 
+      height: 2
+    },
+    shadowRadius: 8
+  },
+  image: {
+    width: 150,
+    height: 150
+  },
+  info: {
+    flex: 1,
+    padding: 12,
+    justifyContent: 'center'
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  status: {
+    fontSize: 14,
+    color: "#c6c6c6ff"
+  },
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  list: {
+    padding: 16
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#f0f0f0'
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    marginRight: 8
+  }
 });
